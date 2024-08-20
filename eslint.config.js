d8e70071a6539e8e928adb765bb3566a478ffed5
Ivan Peresadenko <ivanperesadenkoo@gmail.com>
2024-08-20 15:55:46 +0300

    Configure eslint checking

---
+++ eslint.config.js
@@ -0,0 +1,52 @@
+// @ts-check
+const eslint = require('@eslint/js');
+const tseslint = require('typescript-eslint');
+const angular = require('angular-eslint');
+
+module.exports = tseslint.config(
+	{
+		files: ['**/*.ts'],
+		extends: [
+			eslint.configs.recommended,
+			...tseslint.configs.recommended,
+			...tseslint.configs.stylistic,
+			...angular.configs.tsRecommended,
+		],
+		processor: angular.processInlineTemplates,
+		rules: {
+			'@angular-eslint/directive-selector': [
+				'error',
+				{
+					type: 'attribute',
+					prefix: 'app',
+					style: 'camelCase',
+				},
+			],
+			'@angular-eslint/component-selector': [
+				'error',
+				{
+					type: 'element',
+					prefix: 'app',
+					style: 'kebab-case',
+				},
+			],
+			'@typescript-eslint/explicit-function-return-type': 'off',
+			'@typescript-eslint/no-explicit-any': 'error',
+			'@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
+			'@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+			'no-console': ['error'],
+			'no-debugger': ['warn'],
+			'no-undef': ['error'],
+			eqeqeq: ['error'],
+			'prefer-const': ['error'],
+			'no-multiple-empty-lines': ['error', { max: 1 }],
+			'no-trailing-spaces': ['error'],
+			'no-param-reassign': ['error'],
+		},
+	},
+	{
+		files: ['**/*.html'],
+		extends: [...angular.configs.templateRecommended, ...angular.configs.templateAccessibility],
+		rules: {},
+	}
+);
